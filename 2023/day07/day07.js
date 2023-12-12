487afd573b31e3f2880c522faa92437602dea698
Łukasz Herzyk <lukasz.herzyk@gmail.com>
2023-12-12 18:31:44 +0100

    day 07 part2

--- 2023/day07/day07.js
+++ 2023/day07/day07.js
@@ -4,14 +4,12 @@ KK677 28
 KTJJT 220
 QQQJA 483`;
 
-// input = require('fs')
-//   .readFileSync(__dirname + '/input.txt')
-//   .toString();
+input = require('fs')
+  .readFileSync(__dirname + '/input.txt')
+  .toString();
 
-const getNumberOFJokers = (hand) => [...hand.matchAll(/J/)].length;
-const isFive = (hand) => {
-  const jokers = getNumberOFJokers(hand);
-};
+const getNumberOFJokers = (hand) => [...hand.matchAll(/J/g)].length;
+
 const isThree = (hand) => {
   const unique = [...new Set(hand.split(''))];
   if (unique.length === 3) {
@@ -75,12 +73,74 @@ const getStrength = (hand) => {
 
   throw new Error('no Strength__  ' + hand);
 };
-const compareTwo = (a, b) => {
+const getStrengthWithJokers = (hand) => {
+  const unique = [...new Set(hand.split(''))].length;
+  const jokers = getNumberOFJokers(hand);
+  if (unique === 1) {
+    // five
+
+    return 7;
+  }
+  if (isFour(hand)) {
+    if (jokers === 1 || jokers === 4) {
+      return 7;
+    }
+    return 6;
+  }
+  if (isFullHouse(hand)) {
+    if (jokers) {
+      return 7;
+    }
+    return 5;
+  }
+  if (isThree(hand)) {
+    if (jokers) {
+      return 6;
+    }
+
+    return 4;
+  }
+  if (isTwo(hand)) {
+    if (jokers === 1) {
+      return 5;
+    }
+    if (jokers === 2) {
+      return 6;
+    }
+    return 3;
+  }
+  if (unique === 4) {
+    // one pair
+
+    if (jokers === 1) {
+      return 4;
+    }
+    if (jokers === 2) {
+      return 4;
+    }
+    return 2;
+  }
+  if (unique === 5) {
+    // high card
+
+    if (jokers) {
+      return 2;
+    }
+    return 1;
+  }
+
+  throw new Error('no Strength__  ' + hand);
+};
+const compareTwo = (a, b, part2) => {
   const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+  const cards2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
 
   let c = 0;
   while (c < 5) {
     if (a[c] !== b[c]) {
+      if (part2) {
+        return cards2.indexOf(a[c]) < cards2.indexOf(b[c]) ? 1 : -1;
+      }
       return cards.indexOf(a[c]) < cards.indexOf(b[c]) ? 1 : -1;
     }
     c++;
@@ -105,7 +165,7 @@ const part1 = () => {
       return prev + curr.bid * (index + 1);
     }, 0);
 };
-// 251927063
+
 console.log('part1:', part1());
 
 const part2 = () => {
@@ -114,17 +174,28 @@ const part2 = () => {
     .map((e) => {
       const [hand, bid] = e.split(' ');
 
-      return { hand, bid: Number(bid), strength: getStrength(hand) };
+      return {
+        hand,
+        bid: Number(bid),
+        strength: getStrengthWithJokers(hand)
+      };
     })
+
     .sort((a, b) => {
       if (a.strength === b.strength) {
-        return compareTwo(a.hand, b.hand);
+        return compareTwo(a.hand, b.hand, true);
       }
       return a.strength - b.strength;
+    })
+    .map((e) => {
+      if (getNumberOFJokers(e.hand)) {
+        return e;
+      }
+      return e;
     })
     .reduce((prev, curr, index) => {
       return prev + curr.bid * (index + 1);
     }, 0);
 };
-// 5905
+
 console.log('part2:', part2());
